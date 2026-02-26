5eac2b609594ac5bf1701f770c73927ae7bca5b0
Nilton Santana d3vB4 <nilton.s.santana@ba.estudante.senai.br>
2026-02-26 20:30:53 -0300

    mais conhecimento

---
+++ scripts/5.if_else_if.js
@@ -0,0 +1,14 @@
+// CONDICIONAL
+
+const idade = 20
+
+if (idade > 18) {
+    console.log("Maioridade.")
+
+} else if (idade>= 14 ) {
+    
+    console.log(" Adolecente.")
+} else {
+
+    console.log("Infância.")
+}
