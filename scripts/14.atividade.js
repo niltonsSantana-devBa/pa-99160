59aa4aa2a95a07385451d928deab7468d4de5f6d
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-02-27 21:35:14 -0300

    vetores e atividade

--- scripts/14.atividade.js
+++ scripts/14.atividade.js
@@ -24,9 +24,17 @@ console.log("Mostre o nome e anota dos alunos com nata abaixo de 7.0.")
 console.log("\nFiltrando nota abaixo de 7.0: ")
 
 const menorque7 = listaDeAlunos.filter(
-    usuario => listaDeAlunos.nota < 7.0
+    listaDeAlunos => listaDeAlunos.nota < 7.0
 )
-menorque7.forEach( usuario =>
-    console.log(`${usuario.nome} tem ${usuario.nota}`)
+menorque7.forEach( listaDeAlunos =>
+    console.log(`${listaDeAlunos.nome} tem ${listaDeAlunos.nota}`)
 )
-// console.log("Mostre apenas o nome dos alunos com nota maior ou igual a 7.0.")
+console.log("Mostre apenas o nome dos alunos com nota maior ou igual a 7.0.")
+
+
+const maiorque7 = listaDeAlunos.filter(
+    listaDeAlunos => listaDeAlunos.nota > 7.0
+)
+menorque7.forEach( listaDeAlunos =>
+    console.log(`${listaDeAlunos.nome} tem ${listaDeAlunos.nota}`)
+)
