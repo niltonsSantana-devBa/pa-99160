90638bbf87b8c7858974446bb79fc7cfb47b9995
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-26 21:19:12 -0300

    mensagen no react

---
+++ primeiro-projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,10 @@
+import'./style.css'
+
+export default function Mensagem(){
+    return(
+        <div  className='mensagem'>
+            <h1>Componete</h1>
+            <p>Este é um componente simples.</p>
+        </div>
+    )
+}
